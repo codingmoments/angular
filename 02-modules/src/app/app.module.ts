5dd7e5466ae46be71265f43facbb35836519a731
Nilesh Wani <nileshwaani@gmail.com>
2025-06-22 16:57:28 +0530

    Migrated TasksComponent to non-standalone component

--- 02-modules/src/app/app.module.ts
+++ 02-modules/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { CardComponent } from './shared/card/card.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent],
+  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, TasksComponent, CardComponent],
+  imports: [BrowserModule, CardComponent, TaskComponent, NewTaskComponent],
 })
 export class AppModule {}
