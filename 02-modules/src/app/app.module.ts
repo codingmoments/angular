f0ad6569d873533186d817dd6bcc149cde01e9c9
Nilesh Wani <nileshwaani@gmail.com>
2025-06-22 18:22:07 +0530

    Created a SharedModule for CardComponent. Used that in AppModule.

--- 02-modules/src/app/app.module.ts
+++ 02-modules/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -18,9 +18,8 @@ import { FormsModule } from '@angular/forms';
     TasksComponent,
     NewTaskComponent,
     TaskComponent,
-    CardComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
