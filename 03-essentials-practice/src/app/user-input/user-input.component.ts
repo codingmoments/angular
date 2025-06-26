02a8eeffd1a658948b4cc330ad9275b530015feb
Nilesh Wani <nileshwaani@gmail.com>
2025-06-26 12:01:29 +0530

    Just practiced the lessons. Moved the data and logic to a service.

--- 03-essentials-practice/src/app/user-input/user-input.component.ts
+++ 03-essentials-practice/src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,15 +10,15 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  private investmentService = inject(InvestmentService);
+
   initialInvestment = signal('0');
   annualInvestment = signal('0');
   expectedReturn = signal('5');
   duration = signal('10');
 
-  calculate = output<InvestmentInput>();
-
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.initialInvestment(),
       annualInvestment: +this.annualInvestment(),
       expectedReturn: +this.expectedReturn(),
