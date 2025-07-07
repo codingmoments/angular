2547f928bf95d9db221b6d763d4744da204cf7c0
Nilesh Wani <nileshwaani@gmail.com>
2025-07-07 12:05:47 +0530

    Cleaned up the interval in ngOnDestroy whenever the component is removed

--- 04-component-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 04-component-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
 
   ngOnInit() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const randomNumber = Math.random();
       if (randomNumber < 0.33) {
         this.currentStatus = 'online';
@@ -21,5 +22,11 @@ export class ServerStatusComponent {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
   }
 }
