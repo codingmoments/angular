e89883090908abb8210f60e8ec4c304621c6774b
Nilesh Wani <nileshwaani@gmail.com>
2025-07-13 17:24:30 +0530

    Implemented custom two-way binding using modern way -- model() function

--- 06-custom-2w-binding/src/app/rect/rect.component.ts
+++ 06-custom-2w-binding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,10 +8,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  @Input({ required: true }) size!: { width: string; height: string };
-  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  size = model.required<{ width: string; height: string }>();
 
   onReset() {
-    this.sizeChange.emit({ width: '200', height: '100' });
+    this.size.set({ width: '200', height: '100' });
   }
 }
