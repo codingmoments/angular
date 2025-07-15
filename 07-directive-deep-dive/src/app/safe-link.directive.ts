3872523e76a0d156724b0af746e252f0a043b38e
Nilesh Wani <nileshwaani@gmail.com>
2025-07-15 13:55:49 +0530

    Created a custom structural directive

---
+++ 07-directive-deep-dive/src/app/safe-link.directive.ts
@@ -0,0 +1,23 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onClick($event)',
+  },
+})
+export class SafeLinkDirective {
+  constructor() {
+    console.log('SafeLinkDirective is active!');
+  }
+
+  onClick(event: MouseEvent) {
+    const wantsToLeave = window.confirm(
+      'You are about to leave this site. Do you want to continue?'
+    );
+    if (!wantsToLeave) {
+      event.preventDefault();
+    }
+  }
+}
