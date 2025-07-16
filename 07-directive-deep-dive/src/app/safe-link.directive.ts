46b1e7128c1e3aa1df2e89b9cfe859d60b45b507
Nilesh Wani <nileshwaani@gmail.com>
2025-07-16 10:33:06 +0530

    Made the directive configurable by using the input() property

--- 07-directive-deep-dive/src/app/safe-link.directive.ts
+++ 07-directive-deep-dive/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,6 +8,8 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  sourceParam = input('myApp');
+
   constructor() {
     console.log('SafeLinkDirective is active!');
   }
@@ -19,5 +21,8 @@ export class SafeLinkDirective {
     if (!wantsToLeave) {
       event.preventDefault();
     }
+    const address = (event.target as HTMLAnchorElement).href;
+    (event.target as HTMLAnchorElement).href =
+      address + '?from=' + this.sourceParam();
   }
 }
