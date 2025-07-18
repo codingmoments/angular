dce1c46a071b42c9cca837486c18e4c0baf5898d
Nilesh Wani <nileshwaani@gmail.com>
2025-07-18 13:19:02 +0530

    Created a structural directive and used <ng-template>, TemplateRef and ViewContainerRef to conditionally render the contents

--- 07-directive-deep-dive/src/app/app.component.ts
+++ 07-directive-deep-dive/src/app/app.component.ts
@@ -4,16 +4,17 @@ import { NgIf } from '@angular/common';
 import { AuthComponent } from './auth/auth.component';
 import { AuthService } from './auth/auth.service';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthDirective } from './auth/auth.directive';
 
-@Component({
+@Component( {
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf],
-})
+  imports: [ AuthComponent, LearningResourcesComponent, AuthDirective ],
+} )
 export class AppComponent {
-  private authService = inject(AuthService);
+  private authService = inject( AuthService );
 
-  isAdmin = computed(() => this.authService.activePermission() == 'admin');
+  isAdmin = computed( () => this.authService.activePermission() == 'admin' );
 }
