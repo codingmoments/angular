f9f513b0cb33553410e5a1e704c886036ddfaa56
Nilesh Wani <nileshwaani@gmail.com>
2025-07-21 13:26:21 +0530

    Made the custom pipe configurable and added the logic to transform celsius into fahrenite and vice versa

--- 08-pipes-deep-dive/src/app/temperature.pipe.ts
+++ 08-pipes-deep-dive/src/app/temperature.pipe.ts
@@ -1,11 +1,34 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform, input, output } from "@angular/core";
 
 @Pipe( {
   name: 'temp',
   standalone: true
 } )
 export class TemperaturePipe implements PipeTransform {
-  transform( value: any, ...args: any[] ) {
-    return `${ value } °C`;
+  transform( value: string | number, inputType: 'C' | 'F', outputType?: 'C' | 'F' ): string {
+    let val: number;
+    if ( typeof value === 'string' ) {
+      val = parseFloat( value );
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    if ( inputType === 'C' && outputType === 'F' ) {
+      outputTemp = val * 1.8 + 32;
+    } else if ( inputType === 'F' && outputType === 'C' ) {
+      outputTemp = ( val - 32 ) / 1.8;
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: '°C' | '°F';
+    if ( !outputType ) {
+      symbol = inputType === 'C' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'C' ? '°C' : '°F';
+    }
+
+    return `${ outputTemp } ${ symbol }`;
   }
 }
