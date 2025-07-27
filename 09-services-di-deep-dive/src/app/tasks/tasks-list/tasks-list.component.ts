b9fdfb293afc92adfaaf6da1ba8c2b870d69f7ad
Nilesh Wani <nileshwaani@gmail.com>
2025-07-27 18:08:18 +0530

    Injected the TasksService using custom token

--- 09-services-di-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
+++ 09-services-di-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
+import { TasksServiceToken } from '../tasks.component';
 
 @Component( {
   selector: 'app-tasks-list',
@@ -11,7 +12,7 @@ import { TasksService } from '../tasks.service';
   imports: [ TaskItemComponent ],
 } )
 export class TasksListComponent {
-  private tasksService = inject( TasksService );
+  private tasksService = inject( TasksServiceToken );
   private selectedFilter = signal<string>( 'all' );
   tasks = computed( () => {
     switch ( this.selectedFilter() ) {  // Use computed to derive tasks based on the selected filter
--- 09-services-di-deep-dive/src/app/tasks/tasks.component.ts
+++ 09-services-di-deep-dive/src/app/tasks/tasks.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, InjectionToken } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
 import { TasksService } from './tasks.service';
+
+export const TasksServiceToken = new InjectionToken<TasksService>( 'tasks-service-token' );
 
 @Component( {
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [ NewTaskComponent, TasksListComponent ],
-  providers: [ TasksService ]
+  providers: [ { provide: TasksServiceToken, useClass: TasksService } ]
 } )
 export class TasksComponent { }
