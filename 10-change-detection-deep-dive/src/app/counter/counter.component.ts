a176f9c060710ee37bc709d3762814357fe5f8f3
Nilesh Wani <nileshwaani@gmail.com>
2025-07-30 11:17:27 +0530

    Used NgZone.runOutsideAngular() to opt out of the Angular's change detection or Zone.js watch mode

--- 10-change-detection-deep-dive/src/app/counter/counter.component.ts
+++ 10-change-detection-deep-dive/src/app/counter/counter.component.ts
@@ -1,27 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, NgZone, OnInit, inject, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
-@Component({
+@Component( {
   selector: 'app-counter',
   standalone: true,
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
-  imports: [InfoMessageComponent],
-})
-export class CounterComponent {
-  count = signal(0);
+  imports: [ InfoMessageComponent ],
+} )
+export class CounterComponent implements OnInit {
+  private zone = inject( NgZone );
+  count = signal( 0 );
+
+  ngOnInit(): void {
+    setTimeout( () => {
+      this.count.set( 0 );
+    }, 5000 );
+
+    this.zone.runOutsideAngular( () => {
+      setTimeout( () => {
+        console.log( 'Timer expired!' );
+      }, 10000 );
+    } );
+  }
 
   get debugOutput() {
-    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    console.log( '[Counter] "debugOutput" binding re-evaluated.' );
     return 'Counter Component Debug Output';
   }
 
   onDecrement() {
-    this.count.update((prevCount) => prevCount - 1);
+    this.count.update( ( prevCount ) => prevCount - 1 );
   }
 
   onIncrement() {
-    this.count.update((prevCount) => prevCount + 1);
+    this.count.update( ( prevCount ) => prevCount + 1 );
   }
 }
