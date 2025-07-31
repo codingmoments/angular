23e80f1003a18273dc24ff1e34a8356767d07707
Nilesh Wani <nileshwaani@gmail.com>
2025-07-31 11:15:09 +0530

    Used service with signal to re-evaluate OnPush components on the events occuring in unrelated components

--- 10-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -1,17 +1,19 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MessagesService } from '../messages.service';
 
-@Component({
+@Component( {
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
-})
+} )
 export class MessagesListComponent {
-  messages = input.required<string[]>();
+  private messagesService = inject( MessagesService );
+  messages = this.messagesService.allMessages;
 
   get debugOutput() {
-    console.log('[MessagesList] "debugOutput" binding re-evaluated.');
+    console.log( '[MessagesList] "debugOutput" binding re-evaluated.' );
     return 'MessagesList Component Debug Output';
   }
 }
