81d9d33a9e1c7067065c17ded4cf1e337760ebcc
Nilesh Wani <nileshwaani@gmail.com>
2025-08-01 09:53:21 +0530

    Cleaned up the subscription on BehaviorSubject when the component is getting removed

--- 10-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component( {
@@ -11,13 +11,17 @@ import { MessagesService } from '../messages.service';
 export class MessagesListComponent implements OnInit {
   private messagesService = inject( MessagesService );
   private cdRef = inject( ChangeDetectorRef );
+  private destroyRef = inject( DestroyRef )
 
   messages: string[] = [];
 
   ngOnInit(): void {
-    this.messagesService.messages$.subscribe( ( messages ) => {
+    const subscription = this.messagesService.messages$.subscribe( ( messages ) => {
       this.messages = messages;
       this.cdRef.markForCheck();
+    } );
+    this.destroyRef.onDestroy( () => {
+      subscription.unsubscribe();
     } );
   }
 
