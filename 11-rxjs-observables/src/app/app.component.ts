e4c5773e556aa4e12e49c7552908a582d7397cb3
Nilesh Wani <nileshwaani@gmail.com>
2025-08-03 14:19:49 +0530

    Added the map() operator on the observable returned by interval()

--- 11-rxjs-observables/src/app/app.component.ts
+++ 11-rxjs-observables/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, map } from 'rxjs';
 
 @Component( {
   selector: 'app-root',
@@ -10,17 +10,13 @@ export class AppComponent implements OnInit {
   private destroyRef = inject( DestroyRef );
 
   ngOnInit() {
-    const subscription = interval( 1000 ).subscribe( {
-      next: ( value ) => {
-        console.log( 'Interval value:', value );
-      },
-      error: ( err ) => {
-        console.error( 'Error occurred:', err );
-      },
-      complete: () => {
-        console.log( 'Interval completed' );
-      }
-    } );
+    const subscription = interval( 1000 )
+      .pipe( map( value => value * 2 ) )
+      .subscribe( {
+        next: ( value ) => {
+          console.log( 'Interval value:', value );
+        }
+      } );
 
     this.destroyRef.onDestroy( () => {
       subscription.unsubscribe();
