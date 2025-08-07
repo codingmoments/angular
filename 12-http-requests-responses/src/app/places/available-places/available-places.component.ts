a86c4b9f00547b549026a520e51af875350f6792
Nilesh Wani <nileshwaani@gmail.com>
2025-08-07 10:14:23 +0530

    Used HttpClient to send the HTTP GET request and log the response body

--- 12-http-requests-responses/src/app/places/available-places/available-places.component.ts
+++ 12-http-requests-responses/src/app/places/available-places/available-places.component.ts
@@ -1,16 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
-@Component({
+@Component( {
   selector: 'app-available-places',
   standalone: true,
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
-})
-export class AvailablePlacesComponent {
-  places = signal<Place[] | undefined>(undefined);
+  imports: [ PlacesComponent, PlacesContainerComponent ],
+} )
+export class AvailablePlacesComponent implements OnInit {
+  private httpClient = inject( HttpClient );
+  private destroyRef = inject( DestroyRef );
+
+  places = signal<Place[] | undefined>( undefined );
+
+  ngOnInit() {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>( 'http://localhost:3000/places')
+      .subscribe( {
+        next: ( responseBody ) => {
+          console.log( responseBody );
+        }
+      } );
+
+    this.destroyRef.onDestroy( () => {
+      subscription.unsubscribe();
+    } );
+  }
 }
