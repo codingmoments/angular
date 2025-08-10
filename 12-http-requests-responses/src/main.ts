d3a74a6040e3c9211656c2a81b6ad728e5a719b9
Nilesh Wani <nileshwaani@gmail.com>
2025-08-10 20:09:55 +0530

    Intercepted the outgoing requests and add set custom header

--- 12-http-requests-responses/src/main.ts
+++ 12-http-requests-responses/src/main.ts
@@ -1,8 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+
+function loggingInterceptor( request: HttpRequest<unknown>, next: HttpHandlerFn ) {
+  const clonedRequest = request.clone({
+    headers: request.headers.set( 'X-DEBUG', 'TESTING' ),
+  });
+  console.log( 'HTTP Request:', clonedRequest );
+  return next( clonedRequest );
+}
 
 bootstrapApplication( AppComponent, {
-  providers: [ provideHttpClient() ]
+  providers: [ provideHttpClient( withInterceptors( [ loggingInterceptor ] ) ) ]
 } ).catch( ( err ) => console.error( err ) );
