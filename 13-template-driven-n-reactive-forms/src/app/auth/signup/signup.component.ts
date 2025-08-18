c3e821f0f79b4bc33f72e55c8376f7c50d9f9c8c
Nilesh Wani <nileshwaani@gmail.com>
2025-08-18 11:25:22 +0530

    Just practiced the lessons. Reactive forms - set up the form in TypeScript class. Connected the form and control objects in TypeScript with template elements

--- 13-template-driven-n-reactive-forms/src/app/auth/signup/signup.component.ts
+++ 13-template-driven-n-reactive-forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
-@Component({
+@Component( {
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
-})
-export class SignupComponent {}
+  imports: [ ReactiveFormsModule ]
+} )
+export class SignupComponent {
+  form = new FormGroup( {
+    email: new FormControl( '', {
+      validators: [ Validators.required, Validators.email ],
+    } ),
+    password: new FormControl( '', {
+      validators: [ Validators.required, Validators.minLength( 6 ) ],
+    } ),
+  } );
+
+  onSubmit() {
+    console.log( this.form.controls.email.value );
+    console.log( this.form.controls.password.value );
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+
+}
