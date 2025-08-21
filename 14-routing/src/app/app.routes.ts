e8663d7b7f9d4a674c5e64c54579d95168a1a83a
Nilesh Wani <nileshwaani@gmail.com>
2025-08-21 10:41:12 +0530

    Just practiced the lessons. Added one more route.

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router"
 import { TasksComponent } from "./tasks/tasks.component"
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 
 export const APP_ROUTES: Routes =
   [
+    {
+      path: '',
+      component: NoTaskComponent
+    },
     {
       path: 'tasks',
       component: TasksComponent
