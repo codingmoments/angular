04d63f95ca5b76b6f6e7f8be4a52372f71b3eec6
Nilesh Wani <nileshwaani@gmail.com>
2025-08-23 13:50:50 +0530

    Configured provideRouter to inject parent route path params into the child route components

--- 14-routing/src/app/app.config.ts
+++ 14-routing/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
 import { APP_ROUTES } from "./app.routes";
 
 export const APP_CONFIG: ApplicationConfig = {
   providers: [
-    provideRouter( APP_ROUTES, withComponentInputBinding() )
+    provideRouter( APP_ROUTES, withComponentInputBinding(), withRouterConfig( {
+      paramsInheritanceStrategy: "always"
+    } ) )
   ]
 };
--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 
-@Component({
+@Component( {
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
-})
+  imports: [ TaskComponent ],
+} )
 export class TasksComponent {
+  userId = input.required<string>();
+
   userTasks: Task[] = [];
 }
