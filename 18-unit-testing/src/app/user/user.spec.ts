f1383ae5523485e2c8b30a579260457795394c4a
Nilesh Wani <nileshwaani@gmail.com>
2025-09-07 13:27:14 +0530

    Added tests to test a service with asynchronous method

--- 18-unit-testing/src/app/user/user.spec.ts
+++ 18-unit-testing/src/app/user/user.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { User } from './user';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
 
 describe( 'User', () => {
   let component: User;
   let userService: UserService;
+  let dataService: DataService
   let fixture: ComponentFixture<User>;
 
   beforeEach( async () => {
@@ -17,6 +19,7 @@ describe( 'User', () => {
     fixture = TestBed.createComponent( User );
     component = fixture.componentInstance;
     userService = fixture.debugElement.injector.get( UserService );
+    dataService = fixture.debugElement.injector.get( DataService );
     fixture.detectChanges();
   } );
 
@@ -38,5 +41,16 @@ describe( 'User', () => {
     it( 'shouldn\'t display the user name if logged in', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect( compiled.querySelector( 'p' )?.textContent ).not.toContain( component.user.name );
+  } );
+
+  it( 'shouldn\'t fetch data successfully if not called asynchronously', () => {
+    spyOn( dataService, 'getDetails' ).and.returnValue( Promise.resolve( 'Data' ) );
+    expect( component.data ).toBeUndefined();
+  } );
+
+  it( 'should fetch data successfully if called asynchronously', async () => {
+    spyOn( dataService, 'getDetails' ).and.returnValue( Promise.resolve( 'Data' ) );
+    await fixture.whenStable();
+    expect( component.data ).toBe( 'Data' );
   } );
 } );
--- 18-unit-testing/src/app/user/user.ts
+++ 18-unit-testing/src/app/user/user.ts
@@ -1,20 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
 
 @Component( {
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
   styleUrl: './user.css',
-  providers: [ UserService ]
+  providers: [ UserService, DataService ]
 } )
 export class User implements OnInit {
   private userService = inject( UserService );
+  private dataService = inject( DataService );
 
   user!: { name: string };
   isLoggedIn = false;
+  data!: string;
 
   ngOnInit(): void {
     this.user = this.userService.user;
+    this.dataService.getDetails().then( ( data: string ) => {
+      this.data = data;
+    } );
   }
 }
