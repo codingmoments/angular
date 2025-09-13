e205bcd7ae9562aa3d5ec5303eb2b318a82b267b
Nilesh Wani <nileshwaani@gmail.com>
2025-09-13 13:37:46 +0530

    Used template-based form for form handling

--- 20-authentication/src/app/auth/auth.component.ts
+++ 20-authentication/src/app/auth/auth.component.ts
@@ -1,32 +1,19 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
-@Component({
+@Component( {
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ FormsModule ],
   templateUrl: 'auth.component.html',
   styleUrl: 'auth.component.css'
-})
+} )
 export class AuthComponent {
-  username = '';
-  password = '';
 
-  onLogin() {
-    if (this.username && this.password) {
-      console.log('Login attempt:', { username: this.username, password: this.password });
-      // Add authentication logic here
-    } else {
-      console.log('Please enter both username and password');
-    }
+  onSignUp( form: NgForm ) {
+
   }
 
-  onSignUp() {
-    if (this.username && this.password) {
-      console.log('Sign up attempt:', { username: this.username, password: this.password });
-      // Add sign up logic here
-    } else {
-      console.log('Please enter both username and password');
-    }
+  onLogin( orm: NgForm ) {
   }
 }
