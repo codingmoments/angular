d8b836105b02a8093a26ee1c3c11d603bd003eaa
Nilesh Wani <nileshwaani@gmail.com>
2025-09-24 17:22:30 +0530

    Created Action and Reducer using alternative ways

--- 22-ngrx/src/app/store/counter.actions.ts
+++ 22-ngrx/src/app/store/counter.actions.ts
@@ -1,6 +1,13 @@
-import { createAction, props } from "@ngrx/store";
+import { Action, createAction, props } from "@ngrx/store";
 
-export const increment = createAction(
-  'Increment',
-  props<{ incrementBy: number }>()
-);
+// export const increment = createAction(
+//   'Increment',
+//   props<{ incrementBy: number }>()
+// );
+
+export class IncrementAction implements Action {
+  readonly type = 'Increment';
+  constructor( public incrementBy: number ) { }
+}
+
+export type CounterActions = IncrementAction;
--- 22-ngrx/src/app/store/counter.reducer.ts
+++ 22-ngrx/src/app/store/counter.reducer.ts
@@ -1,9 +1,17 @@
-import { createReducer, on } from "@ngrx/store";
-import { increment } from "./counter.actions";
+import { Action } from "@ngrx/store";
+import { CounterActions, IncrementAction } from "./counter.actions";
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-  initialState,
-  on( increment, (state, action) => state + action.incrementBy )
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on( increment, (state, action) => state + action.incrementBy )
+// );
+
+export function counterReducer( state = initialState, action: CounterActions | Action ) {
+  if ( action.type === 'Increment' ) {
+    return state + (action as IncrementAction).incrementBy;
+  }
+
+  return state;
+}
