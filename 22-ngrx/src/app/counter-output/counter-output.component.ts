c8f80e704744a59b812b19424ee508f86e36feca
Nilesh Wani <nileshwaani@gmail.com>
2025-09-24 18:49:19 +0530

    Created a complex Selector using createSelector function

--- 22-ngrx/src/app/counter-output/counter-output.component.ts
+++ 22-ngrx/src/app/counter-output/counter-output.component.ts
@@ -3,6 +3,7 @@ import { Observable, Subscription } from 'rxjs';
 
 import { Store } from '@ngrx/store';
 import { AsyncPipe } from '@angular/common';
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
 
 @Component( {
   selector: 'app-counter-output',
@@ -14,8 +15,10 @@ import { AsyncPipe } from '@angular/common';
 export class CounterOutputComponent {
   private store = inject( Store<{ counter: number }> );
   count$?: Observable<number>;
+  doubleCount$?: Observable<number>;
 
   constructor() {
-    this.count$ = this.store?.select( state => state.counter );
+    this.count$ = this.store.select( selectCount );
+    this.doubleCount$ = this.store.select( selectDoubleCount );
   }
 }
