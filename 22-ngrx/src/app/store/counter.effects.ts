368cab093036af67ab4fd9c195994f93c8e482ea
Nilesh Wani <nileshwaani@gmail.com>
2025-09-25 15:10:44 +0530

    Defined an Effect using createEffect()

---
+++ 22-ngrx/src/app/store/counter.effects.ts
@@ -0,0 +1,26 @@
+import { inject } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Store } from "@ngrx/store";
+import { tap, withLatestFrom } from "rxjs";
+import { decrement, increment } from "./counter.actions";
+import { selectCount } from "./counter.selectors";
+
+export class CounterEffects {
+
+  private actions$ = inject( Actions );
+  private store = inject( Store<{ counter: number }> );
+
+  saveCount = createEffect( () => {
+    return this.actions$.pipe(
+      ofType( increment, decrement ),
+      withLatestFrom( this.store.select( selectCount ) ),
+      tap( ( [ action, currentCounter ] ) => {
+        console.log( 'Action :', action.type );
+        localStorage.setItem(
+          'count',
+          currentCounter.toString()
+        );
+      } )
+    );
+  }, { dispatch: false } );
+}
