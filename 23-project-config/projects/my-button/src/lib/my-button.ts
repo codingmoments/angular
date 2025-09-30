e9f9ebc903787c680eb02a13927261147f5aa1f0
Nilesh Wani <nileshwaani@gmail.com>
2025-09-30 19:41:38 +0530

    Created a new library within same Angular project folder using the command -- ng generate library my-button

---
+++ 23-project-config/projects/my-button/src/lib/my-button.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-button',
+  imports: [],
+  template: `
+    <p>
+      my-button works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyButton {
+
+}
